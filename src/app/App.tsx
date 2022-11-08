17a083107fbb11241fdf6b694196fe2d01c30e29
Maryia <97767940+Masyak95@users.noreply.github.com>
2022-11-08 21:51:17 +0100

    nesting

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 
 import './App.css';
-import {NavLink, Route, Routes} from "react-router-dom";
+import {NavLink, Outlet, Route, Routes} from "react-router-dom";
 
 
 function App() {
-  return (
-    <div className="App">
-      <NavLink to={'/'}>main</NavLink>---
-      <NavLink to={'/login'}>login</NavLink>---
-      <NavLink to={'/profile'}>profile</NavLink>
+    return (
+        <div className="App">
+            <NavLink to={'/'}>main</NavLink>---
+            <NavLink to={'/login'}>login</NavLink>---
+            <NavLink to={'/profile'}>profile</NavLink>---
+            <NavLink to={'/profile/setting'}>setting</NavLink>---
 
-      <Routes>
-        <Route path={'/'} element={<div>main</div>}/>
-        <Route path={'/login'} element={<div>login</div>}/>
-        <Route path={'/profile'} element={<div>profile</div>}/>
-      </Routes>
-    </div>
-  );
+            <Routes>
+                <Route path={'/*'} element={<div>main</div>}/>
+                <Route path={'/login'} element={<div>login</div>}/>
+                <Route path={'/profile/setting'} element={
+                    <div>
+                        profile
+                        <Outlet/>
+                    </div>
+                }/>
+                <Route path={'*'} element={<div>profile page not found</div>}/>
+                <Route path={'/login'} element={<div>login</div>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
