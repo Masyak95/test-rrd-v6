b2231290cc12be477788ee1ac3d2523e6a1304ed
Maryia <97767940+Masyak95@users.noreply.github.com>
2022-11-08 15:42:59 +0100

    introduction

---
+++ src/app/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './App.css';
+import {NavLink, Route, Routes} from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavLink to={'/'}>main</NavLink>---
+      <NavLink to={'/login'}>login</NavLink>---
+      <NavLink to={'/profile'}>profile</NavLink>
+
+      <Routes>
+        <Route path={'/'} element={<div>main</div>}/>
+        <Route path={'/login'} element={<div>login</div>}/>
+        <Route path={'/profile'} element={<div>profile</div>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
